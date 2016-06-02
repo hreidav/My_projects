3e57681f235195cab28ee851c6b24fe20f282793
vhreida <v.greyda@gmail.com>
2016-06-03 00:03:24 +0300

    Create task07.js

---
+++ task07.js
@@ -0,0 +1,8 @@
+// Task07 - create a function with name task07
+// Function should return any string value
+
+// TODO: Define your function here
+function task07() {
+	return 'Dynamo is champion!'
+}
+alert (task07 ());
