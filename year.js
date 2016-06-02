01d68d51006db39d1a94ef98207631ac3ec35589
vhreida <v.greyda@gmail.com>
2016-06-02 21:49:17 +0300

    Update year.js

--- year.js
+++ year.js
@@ -1,10 +1,17 @@
 <script>
-var year=prompt(year);
-var y=year/4;
 
-if (parseInt(y)!=y) {
+var year=prompt(year);
+if (parseInt(year/4)!=year/4) {
+alert(year+"-th year is not intercalary");
+}
+else if (parseInt(year/100)!=year/100) {
+alert(year+"-th year is intercalary");
+}
+else if (parseInt(year/100)!=year/100||parseInt(year/400)!=year/400) {
 alert(year+"-th year is not intercalary");
 }
 else
-{alert(year+"-th year is intercalary");}
+{alert(year+"-th year is intercalary");
+}
+
 </script>
