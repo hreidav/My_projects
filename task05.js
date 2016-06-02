e6441e4efbc559bf4716caf08cd4c17f3c3d2c07
vhreida <v.greyda@gmail.com>
2016-06-03 00:02:00 +0300

    Create task05.js

---
+++ task05.js
@@ -0,0 +1,13 @@
+// Task05 - Write a code that take 3 digits and create string - 
+// -- first and second should be add and it sum concatenate with third digit as a string
+// Use coma (",") as a separator
+// For example: 
+// Inputed value are 2, 5 and 7
+// You should return a next string - "7,7"
+
+<script>
+function task05(first, second, third) {
+   return first + second + ', ' + String (third);
+}
+alert (task05 (10, 15, 20));
+</script>
