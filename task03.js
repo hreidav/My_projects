e3acccf2912e9cd86e958269cb503a7c8c6425f3
vhreida <v.greyda@gmail.com>
2016-06-03 00:00:55 +0300

    Create task03.js

---
+++ task03.js
@@ -0,0 +1,10 @@
+// Task03 - Write a code that returns concatenation of 2 inputted strings
+// Use space (" ") as a string separator
+// For example: 
+// first is a string "Hello", second is a string "world!"
+// You should return a string "Hello world!"
+
+function conc (first, second) {
+    return first.concat(second);
+}
+alert(conc('task',' 03'))
