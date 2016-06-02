a4f5c1f61206b0706ed13c64757344b3b8b24b54
vhreida <v.greyda@gmail.com>
2016-06-03 00:04:08 +0300

    Create task08.js

---
+++ task08.js
@@ -0,0 +1,12 @@
+// Task08 - create function with name task08
+// Function should pass two parameters 
+// and return number - sum of input parameters
+// Note: strings should be casted to Number type
+
+// TODO: Define your function here
+function task08(x, y){
+   return   +x + +y;
+}
+x=+prompt ('input x ');
+y=+prompt ('input y ');
+alert(task08(x,y));
